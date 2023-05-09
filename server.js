a99508ab9b72677e4e646b0c45eccd9f4e7806d6
Yueun Kang <jkang003@email.unc.edu>
2023-05-09 16:35:14 -0400

    modfiying server.js

--- server.js
+++ server.js
@@ -71,6 +71,85 @@ app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:htt
 // Serve static files
 const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
 app.use('/', express.static(staticpath))
+
+
+import {rps, rpsls} from "./lib/rpsls.js";
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/app', (req, res) => {
+    res.status(200).send('200 OK').end();
+});
+
+/**
+ * Random rps shot
+ */
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
+})
+
+/**
+ * Random rpsls shot
+ */
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
+})
+
+/**
+ * URLEncoded
+ * Note: This is a GET
+ * Example: http://localhost:5000/app/rps/play?shot=rock
+ */
+app.get('/app/rps/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
+})
+
+/**
+ * URLEncoded
+ * Note: This is a GET
+ * Example: http://localhost:5000/app/rps/play?shot=spock
+ */
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot))).end();
+})
+
+/**
+ * JSON
+ * Note: This is a POST
+ */
+app.post('/app/rps/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
+})
+
+/**
+ * JSON
+ * Note: This is a POST
+ */
+app.post('/app/rpsls/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
+})
+
+/**
+ * /:shot syntax allows for that field to be parsed
+ */
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot))).end();
+})
+
+/**
+ * /:shot syntax allows for that field to be parsed
+ */
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot))).end();
+})
+
+/**
+ * Default (non-existent) endpoint
+ */
+app.all('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND').end();
+})
+
 // Create app listener
 const server = app.listen(port)
 // Create a log entry on start
